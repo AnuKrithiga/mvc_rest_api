5c5f5f8bc45d43e58b0c3f28c86981980019d257
anu.krithiga <anu.krithiga@sonicautomotive.com>
2022-11-14 16:38:12 +0530

    Get all & Get API implementation

---
+++ WebApiDemo/WebApiDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiDemo.Service;
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        private EmployeeRepository employeeRepository;
+        public EmployeesController()
+        {
+            this.employeeRepository = new EmployeeRepository();
+        }
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return employeeRepository.GetAllEmployees();
+        }
+
+        public IHttpActionResult GetEmployee(int id)
+        {
+            var employee = employeeRepository.GetEmployee(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+    }
+}
---
+++ WebApiDemo/WebApiDemo/Service/EmployeeRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Service
+{
+    public class EmployeeRepository
+    {
+        Employee[] employees = new Employee[]
+            {
+            new Employee
+            {
+                ID = 1,
+                Name = "Mark",
+                JoiningDate = DateTime.Parse(DateTime.Today.ToString()),
+                Age = 30
+            },
+            new Employee
+            {
+                ID = 2,
+                Name = "Allan",
+                JoiningDate = DateTime.Parse(DateTime.Today.ToString()),
+                Age = 35
+            },
+            new Employee
+            {
+                ID = 3,
+                Name = "Johny",
+                JoiningDate = DateTime.Parse(DateTime.Today.ToString()),
+                Age = 21
+            }
+        };
+
+        public Employee[] GetAllEmployees()
+        {
+            return employees;
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            var employee = employees.FirstOrDefault((p) => p.ID == id);
+            return employee;
+        }
+    }
+}
