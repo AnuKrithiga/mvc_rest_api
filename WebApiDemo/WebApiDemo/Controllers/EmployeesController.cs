5a2989b83ffe4def570656dd746f38fd52ec15d5
anu.krithiga <anu.krithiga@sonicautomotive.com>
2022-11-14 18:38:35 +0530

    initial changes

--- WebApiDemo/WebApiDemo/Controllers/EmployeesController.cs
+++ WebApiDemo/WebApiDemo/Controllers/EmployeesController.cs
@@ -1,22 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using WebApiDemo.Service;
 using WebApiDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 namespace WebApiDemo.Controllers
 {
     public class EmployeesController : ApiController
     {
         private EmployeeRepository employeeRepository;
+        private EmpDBContext db = new EmpDBContext();
+
         public EmployeesController()
         {
             this.employeeRepository = new EmployeeRepository();
         }
-        public IEnumerable<Employee> GetAllEmployees()
+        /*public IEnumerable<Employee> GetAllEmployees()
         {
             return employeeRepository.GetAllEmployees();
         }
@@ -29,6 +31,43 @@ namespace WebApiDemo.Controllers
                 return NotFound();
             }
             return Ok(employee);
+        }*/
+
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult Create(Employee emp)
+        {
+            try
+            {
+                db.Employees.Add(emp);
+                db.SaveChanges();
+                return Ok(emp);
+            }
+            catch
+            {
+                return InternalServerError();
+            }
         }
+
+
+        // PUT: Employee/Edit/5
+        [System.Web.Http.HttpPut]
+        public IHttpActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                var employee = db.Employees.Single(m => m.ID == id);
+                if (employee != null)
+                {
+                    db.SaveChanges();
+                    return Ok();
+                }
+                return NotFound();
+            }
+            catch
+            {
+                return InternalServerError();
+            }
+        }
+
     }
 }
--- WebApiDemo/WebApiDemo/Models/Employee.cs
+++ WebApiDemo/WebApiDemo/Models/Employee.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using System.Data.Entity;
+
 namespace WebApiDemo.Models
 {
     public class Employee
@@ -11,5 +13,12 @@ namespace WebApiDemo.Models
         public string Name { get; set; }
         public DateTime JoiningDate { get; set; }
         public int Age { get; set; }
+    }
+
+    public class EmpDBContext : DbContext
+    {
+        public EmpDBContext()
+        { }
+        public DbSet<Employee> Employees { get; set; }
     }
 }
