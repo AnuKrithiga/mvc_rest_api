f2cdb7da0100327bca9dda8d4d42d56c1801553b
Krithiga S <Anu.KrithigaS@crowe.com>
2022-12-01 18:27:09 +0530

    UT framework skeleton

---
+++ WebApiDemo/WebApiDemo.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WebApiDemo.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Crowe LLP")]
+[assembly: AssemblyProduct("WebApiDemo.Test")]
+[assembly: AssemblyCopyright("Copyright © Crowe LLP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("84b2128d-8509-402d-a715-70a99c51ffd4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebApiDemo/WebApiDemo.Test/WebApiDemoTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace WebApiDemo.Test
+{
+    [TestClass]
+    public class WebApiDemoTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
